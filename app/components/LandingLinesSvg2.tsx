0c6913ea97fc4fb30f84a3cc118f74f8624101b5
Sihan Li <5ih4n.san@gmail.com>
2024-07-31 04:05:52 +0200

    landinglines theme compatibility and second carousel changes

--- app/components/LandingLinesSvg2.tsx
+++ app/components/LandingLinesSvg2.tsx
@@ -18,6 +18,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <g filter="url(#a)">
       <motion.path
         fill="url(#b)"
+        className={"lineColor"}
         d="M308 67.333a2.667 2.667 0 1 0 0 5.334 2.667 2.667 0 0 0 0-5.334Zm-.354 721.021a.502.502 0 0 0 .708 0l3.182-3.182a.502.502 0 0 0-.708-.708L308 787.293l-2.828-2.829a.502.502 0 0 0-.708.708l3.182 3.182ZM307.5 70v44.875h1V70h-1Zm0 134.625v89.75h1v-89.75h-1Zm0 179.5v89.75h1v-89.75h-1Zm0 179.5v89.75h1v-89.75h-1Zm0 179.5V788h1v-44.875h-1Z"
         animate={{ y:-100, }}
         transition={{delay: 0.7, repeat: Infinity, repeatType: "reverse", repeatDelay: 3.3}}
@@ -26,6 +27,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <g filter="url(#c)">
       <motion.path
         fill="url(#d)"
+        className={"lineColor"}
         d="M8 121.333a2.667 2.667 0 1 0 0 5.334 2.667 2.667 0 0 0 0-5.334Zm-.354 603.021a.5.5 0 0 0 .708 0l3.182-3.182a.501.501 0 0 0-.708-.708L8 723.293l-2.828-2.829a.5.5 0 0 0-.708.708l3.182 3.182ZM7.5 124v37.5h1V124h-1Zm0 112.5v75h1v-75h-1Zm0 150v75h1v-75h-1Zm0 150v75h1v-75h-1Zm0 150V724h1v-37.5h-1Z"
         animate={{ y:100}}
         transition={{delay: 0.6, repeat: Infinity, repeatType: "reverse", repeatDelay: 3.3}}
@@ -34,6 +36,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <g filter="url(#e)">
       <motion.path
         fill="url(#f)"
+        className={"lineColor"}
         d="M908 .333a2.667 2.667 0 1 0 0 5.334 2.667 2.667 0 0 0 0-5.334Zm-.354 882.021a.502.502 0 0 0 .708 0l3.182-3.182a.502.502 0 0 0-.708-.708L908 881.293l-2.828-2.829a.502.502 0 0 0-.708.708l3.182 3.182ZM907.5 3v43.95h1V3h-1Zm0 131.85v87.9h1v-87.9h-1Zm0 175.8v87.9h1v-87.9h-1Zm0 175.8v87.9h1v-87.9h-1Zm0 175.8v87.9h1v-87.9h-1Zm0 175.8V882h1v-43.95h-1Z"
         animate={{ y:-100}}
         transition={{delay: 0.9, repeat: Infinity, repeatType: "reverse", repeatDelay: 3.3}}
@@ -42,6 +45,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <g filter="url(#g)">
       <motion.path
         fill="url(#h)"
+        className={"lineColor"}
         d="M608 7.333a2.667 2.667 0 1 0 0 5.334 2.667 2.667 0 0 0 0-5.334Zm-.354 882.021a.502.502 0 0 0 .708 0l3.182-3.182a.502.502 0 0 0-.708-.708L608 888.293l-2.828-2.829a.502.502 0 0 0-.708.708l3.182 3.182ZM607.5 10v43.95h1V10h-1Zm0 131.85v87.9h1v-87.9h-1Zm0 175.8v87.9h1v-87.9h-1Zm0 175.8v87.9h1v-87.9h-1Zm0 175.8v87.9h1v-87.9h-1Zm0 175.8V889h1v-43.95h-1Z"
         transition={{delay: 0.8, repeat: Infinity, repeatType: "reverse", repeatDelay: 3.3}}
         animate={{ y:100}}
@@ -50,6 +54,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <g filter="url(#i)">
       <motion.path
         fill="url(#j)"
+        className={"lineColor"}
         d="M1208 60.333a2.671 2.671 0 0 0-2.67 2.667c0 1.473 1.2 2.667 2.67 2.667s2.67-1.194 2.67-2.667c0-1.473-1.2-2.667-2.67-2.667Zm-.35 721.021c.19.195.51.195.7 0l3.19-3.182a.511.511 0 0 0 0-.708.504.504 0 0 0-.71 0l-2.83 2.829-2.83-2.829a.504.504 0 0 0-.71 0 .511.511 0 0 0 0 .708l3.19 3.182ZM1207.5 63v44.875h1V63h-1Zm0 134.625v89.75h1v-89.75h-1Zm0 179.5v89.75h1v-89.75h-1Zm0 179.5v89.75h1v-89.75h-1Zm0 179.5V781h1v-44.875h-1Z"
         transition={{delay: 1, repeat: Infinity, repeatType: "reverse", repeatDelay: 3.3}}
         animate={{ y:100}}
@@ -58,6 +63,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <g filter="url(#k)">
       <motion.path
         fill="url(#l)"
+        className={"lineColor"}
         d="M1508 114.333a2.671 2.671 0 0 0-2.67 2.667c0 1.473 1.2 2.667 2.67 2.667s2.67-1.194 2.67-2.667c0-1.473-1.2-2.667-2.67-2.667Zm-.35 603.021c.19.195.51.195.7 0l3.19-3.182a.511.511 0 0 0 0-.708.504.504 0 0 0-.71 0l-2.83 2.829-2.83-2.829a.504.504 0 0 0-.71 0 .511.511 0 0 0 0 .708l3.19 3.182ZM1507.5 117v37.5h1V117h-1Zm0 112.5v75h1v-75h-1Zm0 150v75h1v-75h-1Zm0 150v75h1v-75h-1Zm0 150V717h1v-37.5h-1Z"
         transition={{delay: 1.1, repeat: Infinity, repeatType: "reverse", repeatDelay: 3.3}}
         animate={{ y:-100}}
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,14 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { Fan } from "./components/FanSvg";
 import { Reveal } from "./components/Reveal";
-import { ChevronDown, Download } from "lucide-react";
+import { ChevronDown, Download, SquareArrowOutUpRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="max-w mx-auto z-[2] bg-background relative pageShadow">
       <div className=" max-w-screen-2xl noNavHeight animate-smoothSlide relative px-52 flex justify-center mx-auto">
-        <div className="block w-full my-auto relative"><LandingLines2 className="z-[-2]"/></div>
+        <div className="block w-full my-auto relative "><LandingLines2 className="z-[-2] lineColor opacity-60"/></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
           <h1 className="text-center text-3xl m-2">Jag är Sihan Li</h1>
           <div className="h-20"><Profession/></div>
@@ -28,7 +29,7 @@ export default function Home() {
       <div className="z-10">
         <Reveal><h2 className="mx-auto w-fit text-3xl my-5">Senaste Projekt</h2></Reveal>
         <Reveal>
-          <Card className="max-w-screen-md mx-auto overflow-hidden h-max my-10">
+          <Card className="max-w-screen-md mx-auto overflow-hidden my-10">
             <CardHeader>
               <Reveal><CardTitle className="text-2xl">Evaporativ Kylning</CardTitle></Reveal>
               <Reveal><CardDescription>En jämförelse med konventionell luftkonditionering</CardDescription></Reveal>
@@ -65,24 +66,24 @@ export default function Home() {
               </Carousel>
             </CardContent>
             <CardFooter className="flex justify-items-center flex-row">
-              <a href="/#">Dokumentation</a>
-              <Download size={16}/>
+              <a href="/#">Ladda Ner arbetet</a>
+              <Download/>
             </CardFooter>
           </Card>
         </Reveal>
 
         <Reveal>
-          <Card className="max-w-screen-md mx-auto overflow-hidden h-max mb-10">
+          <Card className="max-w-screen-md mx-auto overflow-hidden my-10">
             <CardHeader>
-              <Reveal><CardTitle className="text-2xl">Evaporativ Kylning</CardTitle></Reveal>
-              <Reveal><CardDescription>En jämförelse med konventionell luftkonditionering</CardDescription></Reveal>
+              <Reveal><CardTitle className="text-2xl">Techarena</CardTitle></Reveal>
+              <Reveal><CardDescription>Bästa Gymnasielag: Team Gnomborgen</CardDescription></Reveal>
             </CardHeader>
             <CardContent>
               <Carousel className="mx-10">
                 <CarouselContent>
                     <CarouselItem>
                     <Image
-                        src="/evaporative2.jpg"
+                        src="/techarena.jpg"
                         alt="Bild på dokumentet"
                         fill={true}
                         priority
@@ -90,13 +91,19 @@ export default function Home() {
                       />
                     </CarouselItem>
                     <CarouselItem>
-                      <Image
-                        src="/gymnasiearbeteTitel.png"
-                        alt="Bild på dokumentet"
-                        height={300}
-                        width={300}
-                        className="mx-auto"
-                      />
+                      <h2>Studenter från LBS gymnasiet i Borås</h2>
+                      <blockquote className="mt-6 border-l-2 pl-6 italic">
+                        "För en modig elevgrupp som med en grym teamkänsla presenterade en bra teknisk
+                         lösning på en hög professionell nivå"
+                      </blockquote>
+                      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                        <li>Jessie Lindberg</li>
+                        <li>Joar Maltesson</li>
+                        <li>Samuel Andersson</li>
+                        <li>Liam Ryden</li>
+                        <li>Sihan Li</li>
+                      </ul>
+                      
                     </CarouselItem>
                     <CarouselItem >
                       <p className="p-2">Mitt gymnasiearbete omfattade en teknisk undersökning om Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -109,7 +116,8 @@ export default function Home() {
               </Carousel>
             </CardContent>
             <CardFooter>
-              <a href="/#">Dokumentation</a>
+              <Link href="#">Heet Gihtub</Link>
+              <SquareArrowOutUpRight />
             </CardFooter>
           </Card>
         </Reveal>
