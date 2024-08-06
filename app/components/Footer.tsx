1c4be09b5c6ec2df44e9c99aa6be14c3f26118cd
Sihan Li <5ih4n.san@gmail.com>
2024-08-06 05:40:50 +0200

    footer added links and heet icon

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,17 +10,35 @@ export const Footer = () => {
     <footer className="w-full p-10 h-4/5 sticky bottom-0 z-[1] bg-secondary dot-grid">
         <div className="flex flex-wrap flex-row justify-around mx-auto max-w-screen-2xl">      
             <div>
-                <h3>Version 1.0</h3>
+                <h3 className="text-2xl font-semibold tracking-tight">Version 1.0</h3>
+            </div>
+            <div>
+                <h3 className="text-2xl font-semibold tracking-tight">Snabba Länkar</h3>
+                <Separator />
+                <div className="flex flex-col mb-2 my-2">
+                    <Link href="#" className="text-base font-medium transition group duration-300" prefetch={false}>
+                        Om 
+                        <span className="block max-w-0 group-hover:max-w-6 transition-all duration-500 h-0.5 bg-current"></span>
+                    </Link>
+                    <Link href="#" className="text-base font-medium transition group duration-300" prefetch={false}>
+                        CV 
+                        <span className="block max-w-0 group-hover:max-w-6 transition-all duration-500 h-0.5 bg-current"></span>
+                    </Link>
+                    <Link href="#" className="text-base font-medium transition group duration-300" prefetch={false}>
+                        Arbeten 
+                        <span className="block max-w-0 group-hover:max-w-14 transition-all duration-500 h-0.5 bg-current"></span>
+                    </Link>
+                </div>
             </div>
             <div className="">
-                <h3 className="text-xl ">Kontakta Mig</h3>
+                <h3 className="text-2xl font-semibold tracking-tight">Kontakta Mig</h3>
                 <Separator />
                 <ul className="">
                     <li className="my-2"><Link href="#" className="text-base font-medium transition group duration-300 light:text-white" prefetch={false}>
                     sihanli2005@gmail.com 
                     <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-current"></span></Link>
                     </li>
-                    <div className="flex flex-row">
+                    <div className="flex flex-row justify-between">
                         <Link href="#" className="text-base font-medium transition group duration-300" prefetch={false}>
                             <Image
                                 src="/discord.webp"
--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
           <h1 className="text-center text-3xl m-2">Jag är Sihan Li</h1>
           <div className="h-20"><Profession/></div>
           <div className="mx-auto w-fit m-2">
-            <a href="/#" className="m-2 contactLink w-40">Kontakta Mig</a>
-            <a href="/#" className="m-2 contactLink w-40">Se Mitt Arbete</a>
+            <a href="/#" className="m-2 text-center contactLink w-40">Kontakta Mig</a>
+            <a href="/#" className="m-2 text-center contactLink w-40">Mina Arbeten</a>
           </div>
         </div>
         <div className="animate-bounce mx-auto w-max absolute bottom-3">
@@ -56,9 +56,19 @@ export default function Home() {
                       />
                     </CarouselItem>
                     <CarouselItem >
-                      <p className="p-2">Mitt gymnasiearbete omfattade en teknisk undersökning om Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Saepe fugiat quas quam aspernatur voluptate veniam culpa, quis
-                        tempora quisquam iusto quaerat molestiae nesciunt est rerum commodi nemo deserunt facere maxime!</p>
+                      <p className="leading-7 [&:not(:first-child)]:my-6">Mitt gymnasiearbete omfattade en teknisk undersökning om evaporativ kylning
+                        Syftet var att besvara följande frågor.
+                      </p>
+                      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                        <li>Vilka mordena förbättringar finns för evaporativ kylning?</li>
+                        <li>Vad för- och nackdelarna?</li>
+                        <li>När kan man tillämpa dessa?</li>
+                        <li>Hur förhåller sig effektiviteten till evaporativ kylning?</li>
+                      </ul>
+                      <p className="leading-7 [&:not(:first-child)]:my-6">
+                        Detta ämne intresserade mig särkilt då jag planerade på att bygga en evaporativ kylare på grund av att min rum var riktigt varmt en sommar. 
+                        Det färdiga arbetet är cirka 25 sidor långt och vann priset för bästa tekniska gymnasiearbete året jag tog studenten.  
+                      </p>
                     </CarouselItem>
                 </CarouselContent>
                 <CarouselPrevious />
@@ -84,11 +94,35 @@ export default function Home() {
                     <CarouselItem>
                     <Image
                         src="/techarena.jpg"
-                        alt="Bild på dokumentet"
-                        fill={true}
+                        alt="Mitt lag från Techarena"
+                        height={700}
+                        width={900}
                         priority
                         className="mx-auto"
                       />
+                    </CarouselItem>
+                    <CarouselItem>
+                    <Image
+                        src="/heet.png"
+                        alt="Heet logga"
+                        height={300}
+                        width={400}
+                        priority
+                        className="mx-auto"
+                      />
+                    </CarouselItem>
+                    <CarouselItem>
+                      <h2 className="border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0" >Tema: Socialt Hållbart Borås</h2>
+                      <p className="leading-7 [&:not(:first-child)]:my-6">
+                        Borås Techarena var ett hackathon jag och mina klasskamrater deltog i udner vårat tredje år på gymnasiet. Vi tävlade mot olika företagslag och 
+                        lag från olika högskolor samt gymnasieskolor om vem som kunde utveckla den bästa tekniska lösningen angående temat. Efter 24 timmars hårt arbete 
+                        tog vårat lag, Gnomborgen, hem priset för bästa gymnasielag. 
+                      </p>
+                      <h3 className=" text-2xl font-semibold tracking-tight">HEET</h3>
+                      <p className="mt-2 leading-7">
+                        Vår lösning var en appen HEET som var en lösning för att hitta sociala evenemang i ens närhet. Namnet HEET härstammar från att
+                        appen visar en karta likte en heatmap där man kan se olika händelser i ens närhet. 
+                      </p>
                     </CarouselItem>
                     <CarouselItem>
                       <h2>Studenter från LBS gymnasiet i Borås</h2>
@@ -103,12 +137,6 @@ export default function Home() {
                         <li>Liam Ryden</li>
                         <li>Sihan Li</li>
                       </ul>
-                      
-                    </CarouselItem>
-                    <CarouselItem >
-                      <p className="p-2">Mitt gymnasiearbete omfattade en teknisk undersökning om Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Saepe fugiat quas quam aspernatur voluptate veniam culpa, quis
-                        tempora quisquam iusto quaerat molestiae nesciunt est rerum commodi nemo deserunt facere maxime!</p>
                     </CarouselItem>
                 </CarouselContent>
                 <CarouselPrevious />
